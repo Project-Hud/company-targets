37f3ab6cf485c9798d6cd6904f7face32602812f
Matthew Harrison-Jones <matthew.harrison-jones@clock.co.uk>
2014-02-12 12:49:47 +0000

    Fix wrong class and chart line width

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -9,10 +9,10 @@
   var percent = earnt / target
     , radianAngle = -((twoPi * percent))
 
-  var extraClass = percent < 100 ? 'foreground--warning' : 'percent--good'
+  var extraClass = percent < 1 ? 'foreground--warning' : 'foreground--good'
 
-  var arcBackground = d3.svg.arc().startAngle(0).innerRadius((height * 0.95) * 0.36).outerRadius((height * 0.95) * 0.46)
-    , arcForeground = d3.svg.arc().startAngle(0).innerRadius((height * 0.95) * 0.33).outerRadius((height * 0.95) * 0.48)
+  var arcBackground = d3.svg.arc().startAngle(0).innerRadius((height * 0.95) * 0.38).outerRadius((height * 0.95) * 0.46)
+    , arcForeground = d3.svg.arc().startAngle(0).innerRadius((height * 0.95) * 0.35).outerRadius((height * 0.95) * 0.48)
     , svg = d3.select('.quarterTarget-countdown').append('svg').attr('width', width).attr('height', height).append('g').attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
 
   var meter = svg.append('g').attr('class', 'progress-meter')
