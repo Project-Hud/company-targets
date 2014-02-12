dae7938aa925c9aa3957a83351ad863164d96ea5
Matthew Harrison-Jones <matthew.harrison-jones@clock.co.uk>
2014-02-12 12:41:02 +0000

    Improvements to the 'right section'
    This includes a working and responsive chart with values

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,1 +1,28 @@
-(function () {})()
+(function () {
+  var twoPi = 2 * Math.PI
+    , $data = $('.quarterTarget-countdown')
+    , width = $data.width()
+    , height = $data.height()
+    , target = $data.attr('data-target')
+    , earnt = $data.attr('data-earnt')
+
+  var percent = earnt / target
+    , radianAngle = -((twoPi * percent))
+
+  var extraClass = percent < 100 ? 'foreground--warning' : 'percent--good'
+
+  var arcBackground = d3.svg.arc().startAngle(0).innerRadius((height * 0.95) * 0.36).outerRadius((height * 0.95) * 0.46)
+    , arcForeground = d3.svg.arc().startAngle(0).innerRadius((height * 0.95) * 0.33).outerRadius((height * 0.95) * 0.48)
+    , svg = d3.select('.quarterTarget-countdown').append('svg').attr('width', width).attr('height', height).append('g').attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')')
+
+  var meter = svg.append('g').attr('class', 'progress-meter')
+  meter.append('path').attr('class', 'background').attr('d', arcBackground.endAngle(twoPi))
+
+  var $progress = meter.append('path').attr('class', 'foreground '+ extraClass)
+
+  console.log(percent, radianAngle)
+
+  $progress.attr('d', arcForeground.endAngle(radianAngle))
+
+})()
+
